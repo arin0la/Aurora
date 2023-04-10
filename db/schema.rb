4a356bb84833ec7acca6d7bdbdc204ec420519a9
Janett Gerrard <mikanchik@gmail.com>
2023-04-10 15:51:27 +0100

    migration to add references to join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_10_140524) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_144921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_140524) do
     t.integer "volume"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "mix_id"
+    t.bigint "sound_id"
+    t.index ["mix_id"], name: "index_mix_sounds_on_mix_id"
+    t.index ["sound_id"], name: "index_mix_sounds_on_sound_id"
   end
 
   create_table "mixes", force: :cascade do |t|
