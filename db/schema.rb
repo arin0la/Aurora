cec37503bf87cb41340da0fff4d57112cfdbdc65
Janett Gerrard <mikanchik@gmail.com>
2023-04-10 15:12:32 +0100

    migration to add user reference on mix table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_07_081845) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_140524) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_07_081845) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_mixes_on_user_id"
   end
 
   create_table "sounds", force: :cascade do |t|
