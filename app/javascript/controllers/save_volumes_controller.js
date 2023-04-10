fd8db6712c27bb4d3e5823ed18619032d3aadff8
Janett Gerrard <mikanchik@gmail.com>
2023-04-10 14:28:35 +0100

    volumes are saved on change and sent as stringified object

--- app/javascript/controllers/save_volumes_controller.js
+++ app/javascript/controllers/save_volumes_controller.js
@@ -2,17 +2,27 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="save-volumes"
 export default class extends Controller {
-  static targets = ["form"]
+  static targets = ["form", "volumesData", "sound"]
 
 
   connect() {
     console.log("hello controller")
+    console.log(this.volumesDataTarget)
     document.querySelectorAll("audio").forEach((ele) => {
       ele.volume = 0;
       ele.autoplay = true;
       ele.play();
       console.log("playing")
     })
+  }
+
+  change() {
+    const volumes = {};
+    this.soundTargets.forEach((ele) => {
+      volumes[`${ele.id}`] = ele.volume
+    });
+    this.volumesDataTarget.value = JSON.stringify(volumes);
+    console.log(this.volumesDataTarget)
   }
 
   send(event) {
@@ -23,6 +33,7 @@ export default class extends Controller {
       volumes[`${ele.id}`] = ele.volume
     })
     console.log(volumes);
-
+    this.volumesDataTarget.value = JSON.stringify(volumes);
+    console.log(this.volumesDataTarget);
   }
 }
--- app/models/sound.rb
+++ app/models/sound.rb
@@ -1,5 +1,5 @@
 class Sound < ApplicationRecord
-  has_one_attached :mp3
-  validates :mp3, presence: true
+  # has_one_attached :mp3
+  validates :name, presence: true
   has_many :mix_sounds
 end
